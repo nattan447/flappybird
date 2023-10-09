ce0ff4ff05dcfb851c7de8594b9b0590a7ded584
nattan447 <nattanferreira58@gmail.com>
2023-10-09 16:48:33 -0300

    I set the touch physic

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Game from "./components/game";
+import { useState } from "react";
 export default function App() {
+  const [startgame, setstargame] = useState(false);
+  function start() {
+    setstargame(!startgame);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.pai}>
+      {!startgame ? (
+        <TouchableOpacity style={styles.btnstart} onPress={start}>
+          <Text style={styles.btntxt}>start</Text>
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.filho}>
+          <Game />
+        </View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  pai: {
+    flex: 1, // Isso permite que o ComponentePai ocupe todo o espaço disponível na tela
+    backgroundColor: "lightgray", // Apenas para visualização
+    alignItems: "center", // Alinhe o filho ao centro horizontalmente
+    justifyContent: "center", // Alinhe o filho ao centro verticalmente
+  },
+  filho: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "blue",
+  },
+
+  btnstart: {
+    backgroundColor: "red",
+    borderRadius: 14,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100,
+    height: 40,
+  },
+  btntxt: {
+    color: "white",
+    fontSize: 14,
+    textTransform: "uppercase",
   },
 });
---
+++ components/game.js
@@ -0,0 +1,95 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  Animated,
+  Button,
+} from "react-native";
+import gmstyle from "../estilos/gmstyle";
+import { useState, useEffect, useRef } from "react";
+export default function Game() {
+  //limite 600 top
+  const [top, SetTop] = useState(new Animated.Value(10));
+  const fallingref = useRef(null);
+  const userclickref = useRef(null);
+  const clickcheck = useRef(undefined);
+
+  const para = () => {
+    if (fallingref.current) {
+      top.setValue(10);
+      fallingref.current.stop();
+    }
+  };
+
+  const userclick = (click) => {
+    if (clickcheck.current) {
+      userclickref.current = Animated.timing(top, {
+        toValue: click,
+        duration: 100,
+        useNativeDriver: false,
+      });
+      userclickref.current.start(({ finished }) => {
+        if (finished) {
+          start();
+        }
+      });
+      clickcheck.current = false;
+    } else clickcheck.current = true;
+  };
+  const start = () => {
+    fallingref.current = Animated.timing(top, {
+      toValue: clickcheck.current ? 800 + 180 : 680,
+      duration: 4000,
+      useNativeDriver: false,
+    });
+    if (fallingref.current) {
+      fallingref.current.start();
+      top.addListener((value) => {
+        const atual = value.value;
+        if (clickcheck.current) {
+          userclick(atual - 180);
+          console.log();
+        }
+        if (atual >= 680) {
+          alert("você perdeu");
+          fallingref.current.stop();
+        }
+      });
+    }
+  };
+  useEffect(() => {
+    start();
+  }, []);
+
+  return (
+    <View style={gmstyle.container}>
+      <Image
+        source={require("../assets/background.png")}
+        style={{ width: "100%", height: "100%", position: "absolute" }}
+      ></Image>
+      <TouchableOpacity onPress={userclick}>
+        <Animated.View
+          style={{
+            backgroundColor: "black",
+            width: 200,
+            height: 200,
+            top: top,
+          }}
+        ></Animated.View>
+      </TouchableOpacity>
+      {/* <TouchableOpacity>
+        <Image
+          source={require("../assets/bird.png")}
+          style={{ height: 100, width: 100 }}
+        ></Image>
+      </TouchableOpacity> */}
+      <Button onPress={para} title="stop"></Button>
+      <Button onPress={start} title="start"></Button>
+      <Text>ola mundo</Text>
+    </View>
+  );
+}
